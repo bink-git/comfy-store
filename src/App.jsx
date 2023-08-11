9b79479a65064571045a69a27357e0d02757e616
BINK <bink@email.ua>
2023-08-11 14:41:39 +0300

    add navbar

---
+++ src/App.jsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { Navbar, Sidebar, Footer } from './components';
+
+import {
+  About,
+  Auth,
+  Cart,
+  Checkout,
+  Error,
+  Home,
+  Private,
+  Products,
+  SingleProduct,
+} from './pages';
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <Sidebar />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route exact path="/about">
+          <About />
+        </Route>
+        <Route exact path="/auth">
+          <Auth />
+        </Route>
+        <Route exact path="/cart">
+          <Cart />
+        </Route>
+        <Route exact path="/checkout">
+          <Checkout />
+        </Route>
+        <Route exact path="/private">
+          <Private />
+        </Route>
+        <Route exact path="/products">
+          <Products />
+        </Route>
+        <Route exact path="/products/:id" children={<SingleProduct />} />
+        <Route exact path="*">
+          <Error />
+        </Route>
+      </Switch>
+      <Footer />
+    </Router>
+  );
+}
+
+export default App;
